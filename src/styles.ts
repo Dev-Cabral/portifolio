8684f91147ffaaef654526036e4322537cbcbe4d
Ronario Cabral <ronariolimacabral@gmail.com>
2023-06-18 14:31:56 -0300

    criando o ambiente de desenvolvimento

---
+++ src/styles.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { createGlobalStyle} from 'styled-components'
+
+const EstiloGlobal = createGlobalStyle`
+  * {
+    margin: 0;
+    paddind: 0;
+  }
+`
+
+export default EstiloGlobal
