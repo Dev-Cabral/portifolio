6754958cc8392bbac4731d415cac6458f2409363
Ronario Cabral <ronariolimacabral@gmail.com>
2023-06-24 17:55:50 -0300

    estilizando a Sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const SideBar = () => (
   <aside>
-    <img src="https://github.com/Dev-Cabral.png" />
-    <Titulo fontSize={19}>Ronário Cabral</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Ronário Cabral</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Dev-Cabral
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Dev | FullStack Java
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
--- src/styles.ts
+++ src/styles.ts
@@ -11,6 +11,7 @@ const EstiloGlobal = createGlobalStyle`
 
   body {
     padding-top: 80px;
+    padding-bottom: 80px;
 
     @media (max-width: 768px) {
       padding-top: 16px;
@@ -25,17 +26,12 @@ max-width: 1024px;
 width: 100%;
 margin: 0 auto;
 display: grid;
-grid-template-columns: 128px auto;
+grid-template-columns: 133px auto;
 column-gap: 56px;
 
 @media (max-width: 768px) {
   max-width: 80%;
   display: block;
-
-}
-
-img {
-  max-width: 100%;
 }
 `;
 
